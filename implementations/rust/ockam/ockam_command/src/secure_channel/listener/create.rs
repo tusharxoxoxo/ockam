fe1c80df266fa2de93f99c9465ee8fbed2146d9f
etorreborre <etorreborre@yahoo.com>
2023-12-13 11:55:31 +0100

    refactor(rust): remove the passing of a redundant vault parameter

--- implementations/rust/ockam/ockam_command/src/secure_channel/listener/create.rs
+++ implementations/rust/ockam/ockam_command/src/secure_channel/listener/create.rs
@@ -34,11 +34,8 @@ pub struct CreateCommand {
     #[arg(short, long, value_name = "IDENTIFIERS")]
     authorized: Option<Vec<Identifier>>,
 
-    /// Name of the Vault that the secure-channel listener will use
-    #[arg(value_name = "VAULT_NAME", long, requires = "identity")]
-    vault: Option<String>,
-
     /// Name of the Identity that the secure-channel listener will use
+    /// If it is different from the default node identity
     #[arg(value_name = "IDENTITY_NAME", long)]
     identity: Option<String>,
 }
@@ -59,12 +56,7 @@ async fn run_impl(
 ) -> miette::Result<()> {
     let node = BackgroundNode::create(ctx, &opts.state, &cmd.node_opts.at_node).await?;
     let req = Request::post("/node/secure_channel_listener").body(
-        CreateSecureChannelListenerRequest::new(
-            &cmd.address,
-            cmd.authorized,
-            cmd.vault,
-            cmd.identity,
-        ),
+        CreateSecureChannelListenerRequest::new(&cmd.address, cmd.authorized, cmd.identity),
     );
     let result = node.tell(ctx, req).await;
     match result {
